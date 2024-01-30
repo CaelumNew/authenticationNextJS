f5b0281b30411f15f93103f10d068361737775f1
Dicky Nainggolan <dicky.nbisnis@gmail.com>
2024-01-30 18:54:35 +0700

    new

---
+++ packages/next-auth/src/lib/env.ts
@@ -0,0 +1,29 @@
+import { NextRequest } from "next/server"
+import type { NextAuthConfig } from "./index.js"
+import { setEnvDefaults as coreSetEnvDefaults } from "@auth/core"
+
+/** If `NEXTAUTH_URL` or `AUTH_URL` is defined, override the request's URL. */
+export function reqWithEnvURL(req: NextRequest): NextRequest {
+  const url = process.env.AUTH_URL ?? process.env.NEXTAUTH_URL
+  if (!url) return req
+  const { origin: envOrigin } = new URL(url)
+  const { href, origin } = req.nextUrl
+  return new NextRequest(href.replace(origin, envOrigin), req)
+}
+
+/**
+ * For backwards compatibility, `next-auth` checks for `NEXTAUTH_URL`
+ * and the `basePath` by default is `/api/auth` instead of `/auth`
+ * (which is the default for all other Auth.js integrations).
+ */
+export function setEnvDefaults(config: NextAuthConfig) {
+  try {
+    const url = process.env.AUTH_URL ?? process.env.NEXTAUTH_URL
+    if (!url) return
+    config.basePath ||= new URL(url).pathname
+  } catch {
+  } finally {
+    config.basePath ||= "/api/auth"
+    coreSetEnvDefaults(process.env, config)
+  }
+}
